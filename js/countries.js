d2f114127284219c14ed1bae785f43582ce7991c
foysal767 <uddinmohammadfoysal981@gmail.com>
2022-08-28 00:53:52 +0600

    first commit

---
+++ js/countries.js
@@ -0,0 +1,39 @@
+const loadCountries = () => {
+    fetch('https://restcountries.com/v3.1/all')
+    .then(res => res.json())
+    .then(data => displayCountries(data))
+}
+const displayCountries = countries => {
+    // for(const country of countries){
+    //     console.log(country)
+    // }
+    const countriesContainer = document.getElementById('countries-container');
+    countries.forEach(country => {
+        const countryDiv = document.createElement('div');
+        countryDiv.classList.add('country');
+        // console.log(country)
+        countryDiv.innerHTML = `
+            <h3>Country: ${country.name.common}</h3>
+            <p>Capital: ${country.capital ? country.capital[0] : 'No Capital'}</p>
+            <button onclick="loadCountryDetail('${country.cca2}')">Display</button>
+        `;
+        countriesContainer.appendChild(countryDiv)
+    })
+}
+const loadCountryDetail = (code) =>{
+    const url = `https://restcountries.com/v3.1/alpha/${code}
+    `
+    // console.log('Country Detail:', code)
+    fetch(url)
+    .then(res => res.json())
+    .then(data => displayCountryDetail(data[0]))
+}
+const displayCountryDetail = country =>{
+    const countryDetail = document.getElementById('country-detail');
+    console.log(country)
+    countryDetail.innerHTML = `
+        <h2>Details: ${country.name.common}</h2>
+        <img src="${country.flags.png}">
+    `
+}
+loadCountries()
